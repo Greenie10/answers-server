3dac59e00b9e208882b8371f436d2c7f491c6936
lauragreen <lollymay@gmail.com>
2019-08-27 16:59:04 +0100

    :tada: Initial commit

---
+++ config.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+require("dotenv").config({ path: "./.env" });
+mongoose.Promise = global.Promise;
+
+const url = process.env.MONGODB_URI_GQT;
+
+mongoose.connect(url, { useNewUrlParser: true });
+mongoose.connection.once("open", () =>
+  console.log(`Connected to mongo at ${url}`)
+);
