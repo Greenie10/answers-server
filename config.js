7235cb8543debb06fae03498990e1502baa2ecff
lauragreen <lollymay@gmail.com>
2019-09-04 12:08:12 +0100

    Reset: Connect to my mongodb and add typeDefs & resolvers

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 require("dotenv").config({ path: "./.env" });
 mongoose.Promise = global.Promise;
 
-const url = process.env.MONGODB_URI_GARDENERS;
+const url = process.env.MONGODB_URL;
 
 mongoose.connect(url, { useNewUrlParser: true });
 mongoose.connection.once("open", () =>
