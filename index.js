1eaa5e25b5073aa6201efd63dd39c719a8828dcb
lauragreen <lollymay@gmail.com>
2019-09-05 20:28:24 +0100

    Turn on introspection and playground

--- index.js
+++ index.js
@@ -36,7 +36,12 @@ const resolvers = {
   }
 };
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  introspection: true,
+  playground: true
+});
 const app = express();
 server.applyMiddleware({ app });
 
